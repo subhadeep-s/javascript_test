d2b70d2db73cf3351aebdf1720c28c392a084020
Subhadeep Saha <84631496+subhadeep9898@users.noreply.github.com>
2024-04-22 17:38:14 +0000

    modified  app.js

--- practice/app.js
+++ practice/app.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+//console.log("Hello World!");
+
+const id = 1234;
+
+//id = 4321; wont happen as the value store inside a constant variable cannot be changed.
+
+
+
+console.log(id);
